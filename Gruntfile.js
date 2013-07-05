aaa121fff67e6b33e7a1fdfecf75ed233a6ef217
John W. Long <john@uservoice.com>
2013-07-05 00:20:57 -0400

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        banner: "//\n\
+// <%= pkg.name %> v<%= pkg.version %> (<%= grunt.template.today('dd-mm-yyyy') %>)\n\
+//\n\
+// The lightweight querySelector and events library perfectly suited for widgets\n\
+// and simple applications or websites.\n\
+//\n\
+// Browser support: IE8+ and other modern browsers (Chrome, Safari, Firefox).\n\
+//\n\
+// Originally created by UserVoice for our widget framework (http://uservoice.com)\n\
+// and extracted into a library by John W. Long (@johnwlong).\n\
+//\n\
+// ---\n\
+//\n\
+// Copyright (c) 2013 John W. Long. Portions contributed by Austin Taylor,\n\
+// Jonathan Novak, and Mark Martin.\n\
+//\n\
+// Permission is hereby granted, free of charge, to any person obtaining a copy\n\
+// of this software and associated documentation files (the \"Software\"), to deal\n\
+// in the Software without restriction, including without limitation the rights\n\
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n\
+// copies of the Software, and to permit persons to whom the Software is furnished\n\
+// to do so, subject to the following conditions:\n\
+//\n\
+// The above copyright notice and this permission notice shall be included in all\n\
+// copies or substantial portions of the Software.\n\
+//\n\
+// THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n\
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n\
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n\
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n\
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n\
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n\
+// SOFTWARE.\n\
+//\n",
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/snake-<%= pkg.version %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> v<%= pkg.version %> (<%= grunt.template.today("dd-mm-yyyy") %>) */\n'
+      },
+      dist: {
+        files: {
+          'dist/snake-<%= pkg.version %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        browser: true,
+        laxcomma: true,
+        multistr: true
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
---
+++ dist/snake-0.0.1.js
@@ -0,0 +1,303 @@
+//
+// snake.js v0.0.1 (05-07-2013)
+//
+// The lightweight querySelector and events library perfectly suited for widgets
+// and simple applications or websites.
+//
+// Browser support: IE8+ and other modern browsers (Chrome, Safari, Firefox).
+//
+// Originally created by UserVoice for our widget framework (http://uservoice.com)
+// and extracted into a library by John W. Long (@johnwlong).
+//
+// ---
+//
+// Copyright (c) 2013 John W. Long. Portions contributed by Austin Taylor,
+// Jonathan Novak, and Mark Martin.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+;(function(window, document, Snake, undefined) {
+
+  function qargs(args) {
+    var el, tagName, className;
+    if (args.length === 1) {
+      el = document; tagName = 'div'; className = arguments[0];
+    } else if (args.length === 2) {
+      if (typeof args[0] === 'string') {
+        el = document; tagName = args[0]; className = arguments[1];
+      } else {
+        el = args[0]; tagName = 'div'; className = args[1];
+      }
+    } else if (args.length === 3) {
+      el = args[0]; tagName = args[1]; className = args[2];
+    } else {
+      throw 'invalid arguments';
+    }
+    return [el, tagName, className];
+  }
+
+  // The $ function (aka snake function) can be used as a cross-browser
+  // querySelector(). Compared to jQuery the functionality it supports is quite
+  // limited, but it works well for some applications.
+  //
+  // In its simplest form it only looks up divs by a class name:
+  //
+  //     $('content')             // Find a div with a class of 'content'
+  //
+  // But you can also pass a specific tag to find other types of tags:
+  //
+  //     $('a', 'external')      // Find a link with a class of 'external'
+  //
+  // It can also be invoked on an element to find child elements:
+  //
+  //     $(el, 'classname')        // Find a div inside of el
+  //     $(el, 'article', 'post')  // Find an article tag with a class of 'post' inside of el
+  //
+  var $ = function() {
+    var args = qargs(arguments)
+    ,   el = args[0]
+    ,   tagName = args[1]
+    ,   className = args[2]
+    ;
+    if (el === document && el.querySelector) {
+      return el.querySelector(tagName + '.' + className);
+    } else {
+      var els = el.getElementsByTagName(tagName)
+      ,   regexp = new RegExp('\b' + className + '\b', 'gmi')
+      ;
+      for (var i=0; i < els.length; i++) {
+        if (regexp.test(els[i].className)) {
+          return els[i];
+        }
+      }
+      return null;
+    }
+  };
+
+  // The $.all function is similar to the $ function except that much like
+  // querySelectorAll() it returns an array of matching elements. In the
+  // event that no elements are found an empty array is returned.
+  //
+  // The $.all function supports the same parameters as the $ function:
+  //
+  //     $$('post')               // All divs with a class name of 'post'
+  //     $$(el, 'post')           // All 'post' divs inside of el
+  //
+  //     $$('a', 'external')      // All links with a class name of 'external'
+  //     $$(el, 'a', 'external')  // All 'external' links inside of el
+  //
+  $.all = function() {
+    var args = qargs(arguments)
+    ,   el = args[0]
+    ,   tagName = args[1]
+    ,   className = args[2]
+    ;
+    if (el === document && el.querySelectorAll) {
+      return el.querySelectorAll(tagName + '.' + className);
+    } else {
+      var els = el.getElementsByTagName(tagName)
+      ,   regexp = new RegExp('\b' + className + '\b', 'gmi')
+      ,   result = []
+      ;
+      for (var i=0; i < els.length; i++) {
+        if (regexp.test(els[i].className)) {
+          result.push(els[i]);
+        }
+      }
+      return result;
+    }
+  };
+
+  var uniqIdCount = 0;
+  function uniqId() {
+    uniqIdCount += 1;
+    return 'snakejs-' + uniqIdCount;
+  }
+
+  // Return an ID for el. Create one if needed.
+  $.identify = function(el) {
+    var id = el.getAttribute('id');
+    if (id) {
+      return id;
+    } else {
+      id = uniqId();
+      el.setAttribute('id', id);
+      return id;
+    }
+  };
+
+  // Associate arbitrary data with el.
+  $.data = (function() {
+    var d = {};
+    function data(el, key, value) {
+      var id = $.identify(el);
+      d[id] = d[id] || {};
+      if (arguments.length > 2) {
+        d[id][key] = value;
+        return value;
+      } else {
+        return d[id][key];
+      }
+    }
+    return data;
+  })();
+
+  // Add a class to el.
+  $.addClass = function(el, name) {
+    if ((new RegExp("\b" + name + "\b")).test(el.className)) return;
+    el.className += (el.className ? ' ' : '') + name;
+  };
+
+  // Remove a class from el.
+  $.removeClass = function(el, name) {
+    el.className = el.className.replace(new RegExp("\b" + name + "\b", "g"), ' ');
+  };
+
+  // Does el have class?
+  $.hasClass = function(el, name) {
+    return el.className.indexOf(name) > -1;
+  };
+
+  // Get the width and height of el.
+  $.dimensions = function(el) {
+    var d = el.display;
+    if (d !== 'none' && d !== null) { // Safari bug
+      return { width: el.offsetWidth, height: el.offsetHeight };
+    }
+
+    // All *Width and *Height properties give 0 on elements with display none,
+    // so enable the element temporarily
+    var s = el.style
+    ,   vis = s.visibility
+    ,   pos = s.position
+    ,   dis = s.display
+    ;
+    s.visibility = 'hidden';
+    s.position = 'absolute';
+    s.display = 'block';
+
+    var w = el.clientWidth
+    ,   h = el.clientHeight
+    ;
+    s.display = dis;
+    s.position = pos;
+    s.visibility = vis;
+
+    return { width: w, height: h };
+  };
+
+  // Get top and left offset of el.
+  $.offset = function(el) {
+    var e = el
+    ,   o = { top: 0, left: 0 }
+    ;
+    while (e) {
+      o.top += e.offsetTop;
+      o.left += e.offsetLeft;
+      e = e.offsetParent;
+    }
+    return o;
+  };
+
+  // Use the $.on function to add an event listener to an element. For example:
+  //
+  //     $.on(el, 'click', function(e) {
+  //       console.log('clicked');
+  //     });
+  //
+  $.on = function(el, name, f) {
+    if ('addEventListener' in el) {
+      el.addEventListener(name, f, false);
+    } else {
+      el.attachEvent('on' + name, function() {
+        return f.call(el, window.event);
+      });
+    }
+  };
+
+  // Use the $.off function to remove an event listener from an element:
+  //
+  //     var listener = function() { ... };
+  //     $.on(el, 'click', listener);
+  //     $.off(el, 'click', listener);
+  //
+  $.off = function(el, name, f) {
+    if ('removeEventListener' in el) {
+      el.removeEventListener(name, f, false);
+    } else {
+      el.detachEvent(name, f);
+    }
+  };
+
+  // Use the $.one to add an event listener that will be removed the first time
+  // it is invoked:
+  //
+  //     $.one(el, 'mousemove', function(e) {
+  //       console.log('This should only ever be logged one time.');
+  //     });
+  //
+  $.one = function(el, name, f) {
+    var wrapper = function() {
+      f(arguments);
+      $.off(el, name, wrapper);
+    };
+    $.on(el, name, wrapper);
+  };
+
+  // Use the $.ready function to add a DOM ready event listener. This listener
+  // will be fired when the DOM has been fully loaded.
+  //
+  //     $.ready(function() {
+  //      console.log('The DOM is loaded!');
+  //     });
+  //
+  $.ready = function(f) {
+    if (document.addEventListener) {
+      if ("complete" === document.readyState) {
+        f();
+      } else {
+        document.addEventListener("DOMContentLoaded", f, false);
+        window.addEventListener("load", f, false);
+      }
+    } else if (document.attachEvent) {
+      document.attachEvent("onreadystatechange", f);
+      window.attachEvent("onload", f);
+      var topf = false;
+      try {
+        topf = (window.frameElement === null);
+      } catch(e) {}
+      if (document.documentElement.doScroll && topf) {
+        var scrollCheck = function() {
+          try {
+            document.documentElement.doScroll("left");
+          } catch(e) {
+            setTimeout(scrollCheck, 1);
+            return;
+          }
+          f();
+        };
+      }
+    }
+  };
+
+  $.noconflict = Snake.noconflict;
+  window.Snake = $;
+  if (!$.noconflict) { window.$ = $; }
+
+})(window, window.document, window.Snake || {});
